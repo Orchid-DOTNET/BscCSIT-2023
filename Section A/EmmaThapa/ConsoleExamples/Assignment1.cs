9e256c7077f119f5d4f0f708863dc7fb8c545ff3
unknown <emma.thapa@hotmail.com>
2023-03-23 21:23:26 +0545

    Added Name folder and Assignment for reference

---
+++ Section A/EmmaThapa/ConsoleExamples/Assignment1.cs
@@ -0,0 +1,16 @@
+namespace ConsoleExamples
+{
+    public class StringEdit
+    {
+        public void DoubleQuote()
+        {
+            Console.WriteLine("[*] Enter a string: ");
+            string value = Console.ReadLine();
+            string[] valArr = value.Split(" ");
+            foreach (string val in valArr)
+            {
+                Console.WriteLine('"' + val + '"');
+            }
+        }
+    }
+}
---
+++ Section A/EmmaThapa/ConsoleExamples/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleExamples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            StringEdit stringEdit = new StringEdit();
+            //Assignment 1
+            stringEdit.DoubleQuote();
+        }
+    }
+}
