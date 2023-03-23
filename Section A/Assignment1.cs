8f9e5d51b489106af5b359d08e1a808d286a4f84
sregmi02 <122499949+sregmi02@users.noreply.github.com>
2023-03-23 16:57:48 +0545

    Add files via upload

---
+++ Section A/Assignment1.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace class1
+{
+    class stringPrint
+    {
+        public static void Main()
+        {
+            Console.WriteLine("\n Enter a String: ");
+            string value = Console.ReadLine();
+            string[] valArr = value.Split(" ");
+            for(int i = 0; i < valArr.Length; i++)
+            {
+                Console.WriteLine('"' + valArr[i] + '"');
+            }
+        }
+    }
+
+}
