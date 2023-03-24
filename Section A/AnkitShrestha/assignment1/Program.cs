3d59a5f732ea91d4e6ffa92d2021dcc389134984
Ankit Shrestha <ankit.shrestha196@gmail.com>
2023-03-24 08:01:31 +0545

    First Assignment

---
+++ Section A/AnkitShrestha/assignment1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter String:");
+
+      string val = Console.ReadLine();
+
+      string[] arrVal = val.Split(" ");
+
+      for (int i = 0; i < arrVal.Length; i++) {
+        Console.WriteLine("\""+ arrVal[i] + "\"");
+      }
+      
+    }
+  }
+}
