143a214ce3aa2b1291a9165fd1eb7af5eec76a8a
nitishat7 <nitishatimaslina7@gmail.com>
2023-03-24 07:53:20 +0545

    Submitted Assignment 1

---
+++ Section A/NitishaTimalsina/Assignment1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class StringEdit
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string value = Console.ReadLine();
+            string[] strArray = value.Split(" ");
+            foreach(string str in strArray)
+            {
+                Console.WriteLine('"'+str+'"');
+            }
+        }
+    }
+}
