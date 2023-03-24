597e9e47b38020a8b78252887d4b9eafa41babe9
raajz49 <koiralaraajz@gmail.com>
2023-03-24 20:32:18 +0545

    Added assignment 1

---
+++ Section A/RajKoirala/assignment1.cs
@@ -0,0 +1,18 @@
+﻿namespace SplitString
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //string 
+            String myString = "dotnet Centric Computing";
+            String[] newString = myString.Split(" ");
+            //loop
+            foreach (String s in newString)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
