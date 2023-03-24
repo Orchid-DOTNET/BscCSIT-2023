f4828c1624a3cccf6145550b1319d5ec8c643468
Saroj Bhandari <sarosebhandari100@gmail.com>
2023-03-25 00:00:11 +0545

    Added Assignments

---
+++ Section A/SarojBhandari/Assignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+public class newProgram {
+   public static void Main() {
+      using (StreamWriter sw = new StreamWriter("hello.txt")) {
+         sw.WriteLine("This is demo line 1");
+         sw.WriteLine("This is demo line 2");
+         sw.WriteLine("This is demo line 3");
+      }
+      int count = File.ReadAllLines("hello.txt").Length;
+      Console.WriteLine("Number of lines: "+count);
+   }
+}
