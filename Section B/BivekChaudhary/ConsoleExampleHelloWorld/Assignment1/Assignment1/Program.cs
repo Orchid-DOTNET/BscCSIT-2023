7af6dfcc9c5fd4588af0f596c8bbbf1d56ed4c1f
unknown <bivekchaudhary798@gmail.com>
2023-03-25 22:23:47 +0545

    added assignment

---
+++ Section B/BivekChaudhary/ConsoleExampleHelloWorld/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace SplitingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[*] Enter a string: ");
+            string value = Console.ReadLine();
+            string[] valArr = value.Split(" ");
+            foreach (string val in valArr)
+            {
+                Console.WriteLine('"'+val+'"');
+            }
+
+        }
+    }
+}
