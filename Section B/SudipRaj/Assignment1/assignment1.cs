f5ecdf1113830625c37de93f5372a32819cf593d
sudip0-0 <sudipshrestha196@gmail.com>
2023-03-25 21:03:19 +0545

    assignment 1

---
+++ Section B/SudipRaj/Assignment1/assignment1.cs
@@ -0,0 +1,20 @@
+﻿//c sharp code that takes input and put double quotes around each word and print it
+using System;
+
+
+namespace assignmnet
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            foreach (string word in words)
+            {
+                Console.Write("\"" + word + "\" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
