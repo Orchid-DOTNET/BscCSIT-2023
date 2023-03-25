8fc5dd6952e369d1a0ebfca00b4c8e5a4a266e8f
Bilish12 <bil.kharbuja100@gmail.com>
2023-03-25 16:52:25 +0545

    Added Assignment 1

---
+++ Section B/BilishKharbuja/assignment1.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleExamples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("[*] Enter a string: ");
+            string value = Console.ReadLine();
+            string[] valArr = value.Split(" ");
+            foreach (string val in valArr)
+            {
+                Console.WriteLine('"' + val + '"');
+            }
+        }
+    }
+}
