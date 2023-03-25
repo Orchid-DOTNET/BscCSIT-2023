e98ca88fa0d9fd668bb276c3cce3659c427cb351
sregmi02 <regmisubrat02@gmail.com>
2023-03-25 17:46:12 +0545

    Assignment 2 added

---
+++ Section A/SubratRegmi/Assignment2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment2
+{
+
+
+    class fileExercise
+    {
+        public void LineCount()
+        {
+            
+            int count = 0;
+            StreamReader file = new StreamReader("myfile.txt");
+            Console.WriteLine("File Content : ");
+            string line = file.ReadLine();
+            while (line != null)
+            {
+               Console.WriteLine(line);
+               count++;
+               line = file.ReadLine();
+            }
+                    
+            Console.Write("\n The number of lines in the file is :" + count);
+            
+        }
+    }
+}
--- Section A/SubratRegmi/Program.cs
+++ Section A/SubratRegmi/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿using Assignment2;
 using Assignment1;
+//assignment 1
 stringHandling sh = new stringHandling();
 sh.stringPrint();
+//assignment 2
+fileExercise fe = new fileExercise();
+fe.LineCount();
