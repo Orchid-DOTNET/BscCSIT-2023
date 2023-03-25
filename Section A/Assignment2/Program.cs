024b9315c997f8d84dd8e34c9f2b236a7e0b5a2c
nitishat7 <nitishatimaslina7@gmail.com>
2023-03-25 20:20:45 +0545

    Assignment 2 added

---
+++ Section A/Assignment2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"D:\a2.txt";
+
+        try
+        {
+            int count = File.ReadLines(filePath).Count();
+
+            Console.WriteLine("The file '{0}' lines.", count);
+        }
+        catch(FileNotFoundException) 
+        {
+            Console.WriteLine("File Not Found!!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
