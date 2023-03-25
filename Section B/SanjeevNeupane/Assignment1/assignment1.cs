24c547fac651a33cfe9fc54092ab656acd529407
unknown <neupanesanjeev675@gmail.com>
2023-03-25 20:12:36 +0545

    assignment1 added

---
+++ Section B/SanjeevNeupane/Assignment1/assignment1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string inputString = Console.ReadLine();
+
+        string[] words = inputString.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = "\"" + words[i] + "\"";
+        }
+
+        Console.WriteLine("Formatted words:");
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+
+        Console.ReadKey();
+    }
+}
