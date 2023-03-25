a37115b83dce17c26c65ca12094ce43e54a594c2
Avishek Majhi <avishekmajhi321@gmail.com>
2023-03-25 12:25:57 +0545

    Assignment Added

---
+++ Section B/Assignment1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace assignment1{
+    class program{
+        static void Main (string[] args){
+            Console.WriteLine("Enter a string: ");
+            string value = Console.ReadLine();
+            string[] valArr= value.Split(" ");
+            foreach (string item in valArr){
+                Console.WriteLine('"'+ item+ '"');
+            }
+            
+        } 
+    }
+}
