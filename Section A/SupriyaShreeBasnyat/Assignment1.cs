ab4b06dfebddac5908dc2627833317550cbec801
unknown <supriyaaa999@gmail.com>
2023-03-25 12:44:49 +0545

    Assignment 1 added

---
+++ Section A/SupriyaShreeBasnyat/Assignment1.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment1
+{
+    class stringToArray
+    {
+        public void converter()
+        {
+            Console.WriteLine("Enter a string:");
+            string value = Console.ReadLine();
+            string[] values = value.Split(' ');
+            foreach (string a in values)
+            {
+                Console.WriteLine('"' + a + '"');
+            }
+
+
+        }
+    }
+}
---
+++ Section A/SupriyaShreeBasnyat/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment1;
+Console.WriteLine("Hello, World!");
+stringToArray one = new stringToArray();
+one.converter();
