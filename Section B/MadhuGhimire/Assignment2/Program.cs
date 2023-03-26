8d8e9dbaffa431ebc5682d394fb1878f33e1bfb3
Madhu Ghimire <ghimiremadhu2058@gmail.com>
2023-03-26 22:17:22 +0545

    Added Assignment 2 and 3

---
+++ Section B/MadhuGhimire/Assignment2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Users\ACER\Desktop\BscCSIT-2023\Section B\MadhuGhimire\Assignment2\linecount.txt";
+        int lineCount = 0;
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            while (reader.ReadLine() != null)
+            {
+                lineCount++;
+            }
+        }
+        Console.WriteLine("The file has {0} lines.", lineCount);
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
+
