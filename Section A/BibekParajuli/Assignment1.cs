f3488409ae808290a2b5930b3b8d3abd7851f7a7
bibekparajuli07 <bibekparajuli29#gmail.com>
2023-03-26 09:59:32 +0545

    My Assignment

---
+++ Section A/BibekParajuli/Assignment1.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Assignment1{
+    class EditString{
+        public static void String_Edit(){
+            Console.WriteLine("Assignment 1");
+        }
+    }
+}
---
+++ Section A/BibekParajuli/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using Assignment1;
+
+
+namespace ConsoleApplication{
+    class CodeRunner{
+        public static void Main(){
+            //Assignment 1
+            EditString.String_Edit();
+        }
+    }
+}
