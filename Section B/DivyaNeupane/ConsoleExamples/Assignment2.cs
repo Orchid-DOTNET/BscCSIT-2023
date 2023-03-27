6dc0f85319729faca27a6131065eeb5d033a471b
unknown <divyaneupane12@gmail.com>
2023-03-27 08:26:01 +0545

    added file

---
+++ Section B/DivyaNeupane/ConsoleExamples/Assignment2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO
+
+namespace ConsoleExamples
+{
+    public class assignment2
+    {
+        public static void Main()
+        {
+            using (StreamWriter sw = new StreamWriter("assignment2.txt"))
+            {
+                sw.WriteLine("This is demo line 1");
+                sw.WriteLine("This is demo line 2");
+                sw.WriteLine("This is demo line 3");
+            }
+            int count = File.ReadAllLines("assignment2.txt").Length;
+            Console.WriteLine("Number of lines: " + count);
+        }
+    }
+}
