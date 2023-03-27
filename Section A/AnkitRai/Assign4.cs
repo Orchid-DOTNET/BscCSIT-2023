0402b4a386e75e6c857235e3bb5c3b5a22f19ae6
Ankit Rai <raia85859@gmail.com>
2023-03-27 17:34:25 +0545

    Assignment 4

---
+++ Section A/AnkitRai/Assign4.cs
@@ -0,0 +1,32 @@
+﻿using MultipleInheritance;
+namespace Assignment4
+{
+    class Calculation: Calc1, Clac2, Calc3, Calce4
+    {
+       public float Add(int a , int b)
+        {
+            return a + b;
+        }
+        public float Sub(int a ,int b)
+        {
+            return a - b;
+
+        }
+        public float Mul(int a ,int b)
+        {
+            return a * b;
+        }
+        public float Div(int a ,int b)
+        {
+            if (b == 0)
+            {
+                return a;
+
+            }
+            else
+            {
+                return (a / b);
+            }
+        }
+    }
+}
---
+++ Section A/AnkitRai/Assignment4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MultipleInheritance
+{
+    interface Calc1
+    {
+        float Add(int x, int y);
+            }
+    interface Clac2
+    {
+        float Sub(int x , int y);
+
+    }
+    interface Calc3
+    {
+        float Mul(int x, int y);
+    }
+    interface Calce4
+    {
+        float Div(int x , int y);
+    }
+ 
+}
