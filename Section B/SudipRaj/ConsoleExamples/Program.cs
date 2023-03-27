9e232328c5b4ae847bd4b95a1ab91b4db3c361b7
sudip0-0 <sudipshrestha196@gmail.com>
2023-03-27 21:43:58 +0545

    Assignment 3 4 and 5

---
+++ Section B/SudipRaj/ConsoleExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Assignment3;
+using Assignment4;
+using Assignment5;
+namespace ConsoleExamples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+            // StringEdit stringEdit = new StringEdit();
+            // //Assignment 1
+            // stringEdit.DoubleQuote();
+            
+            //Assignment3 
+            LinqClass linqClass = new LinqClass();
+            linqClass.EmployeeFunc();
+        //Assignment4
+            BasicCal calObj = new BasicCal();
+            calObj.BasicCalFunc();
+
+            //Assignment5
+            ExceptionHandling exceptionHandling = new ExceptionHandling();
+            exceptionHandling.ExceptionFunc();
+
+        }
+    }
+}
