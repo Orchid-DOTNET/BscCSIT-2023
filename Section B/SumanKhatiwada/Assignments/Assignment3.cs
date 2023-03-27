8b556b334351a45a257703aa7b0f934ffcfa718f
rad <iamsumankht@gmail.com>
2023-03-27 22:38:16 +0545

    Assignment 3

---
+++ Section B/SumanKhatiwada/Assignments/Assignment3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Employee
+{
+    int ID;
+    string Name;
+    int Age;
+
+    public static void displayEmployee()
+    {
+        List<Employee> employee = new List<Employee>()
+        {
+            new Employee { ID=4, Name="Suman", Age=19},
+            new Employee { ID=3, Name="Ram", Age=32},
+            new Employee { ID=6, Name="Shyam", Age=21},
+            new Employee { ID=1, Name="Hari", Age=36},
+            new Employee { ID=9, Name="Abhishek", Age=19},
+            new Employee { ID=2, Name="Amir", Age=39},
+        };
+
+        IEnumerable<Employee> Query =
+            from emp in employee
+            orderby emp.ID
+            where emp.Age>30
+            select emp;
+
+        Console.WriteLine("ID | Name | Age | Salary");
+        Console.WriteLine("-------------------");
+        foreach (Employee e in Query)
+        {
+            Console.WriteLine($"{e.ID} {e.Name} {e.Age}");
+        }
+    }
+}
--- Section B/SumanKhatiwada/Assignments/Program.cs
+++ Section B/SumanKhatiwada/Assignments/Program.cs
@@ -1,12 +1,16 @@
 ﻿using Assignment1;
 
+
 namespace Assignments
 {
     class AllAssignments
     {
         static void Main(string[] args)
         {
-            Assignment1.EditString.StringEdit();
+            /* Assignment1.EditString.StringEdit(); */
+
+            Employee.displayEmployee();
+
         }
     }
 }
