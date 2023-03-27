bb047aab6c23cfcd573d0dd4b7e3b1b6ab86b96d
sregmi02 <regmisubrat02@gmail.com>
2023-03-27 20:18:13 +0545

    Assignment 4 Added

--- Section A/SubratRegmi/Program.cs
+++ Section A/SubratRegmi/Program.cs
@@ -8,6 +8,15 @@ using Assignment3;
 //fileExercise fe = new fileExercise();
 //fe.LineCount();
 //assignment 3
-
-EmployeeDetails emp = new EmployeeDetails();
-emp.empDetails();
+//EmployeeDetails emp = new EmployeeDetails();
+//emp.empDetails();
+//assignment4
+Calculator c = new Calculator();
+var addition = c.add(10, 2);
+var subtract = c.sub(10, 2);
+var divide = c.div(10, 2);
+var multiply = c.mul(10, 2);
+Console.WriteLine("Addition: {0}",addition);
+Console.WriteLine("Subtraction: {0}", subtract);
+Console.WriteLine("Division: {0}",divide);
+Console.WriteLine("Multiplication: {0}", multiply);
