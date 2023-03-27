b1ab052b1013293c648423dce127cffa54ff1e28
unknown <adarsh.gautam888@gmail.com>
2023-03-27 20:02:02 +0545

    assignment4 added

---
+++ Section B/AdarshGautam/assignment4.cs
@@ -0,0 +1,21 @@
+﻿namespace multipleinheritance{
+    interface Cal1 {
+    int Add(int a, int b);
+}
+
+
+interface Cal2 {
+    int Subtract(int a, int b);
+}
+
+
+interface Cal3 {
+    int Multiply(int a, int b);
+}
+
+
+interface Cal4 {
+    int Divide(int a, int b);
+}
+    
+}
