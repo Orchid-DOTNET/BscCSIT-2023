838497a936f32ebdea4af88231b7574e2284dee1
unknown <amishasainju@gmail.com>
2023-03-27 18:07:38 +0545

    added assignment

---
+++ Section B/AmishaSainju/ConsoleExamples/Assignment1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Assignment1
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = "\"" + words[i] + "\"";
+        }
+
+        string output = string.Join(" ", words);
+
+        Console.WriteLine(output);
+    }
+}
---
+++ Section B/AmishaSainju/ConsoleExamples/Assignment2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleExamples
+{
+    internal class Assignment2
+    {
+        static void Main()
+        {
+            string filePath = "D:\\NET\\BscCSIT-2023\\Section B\\AmishaSainju\\ConsoleExamples\\Assignment2.txt";
+
+            int Counter = 0;
+            
+
+            using (StreamReader studyline = new StreamReader(filePath))
+            {
+                while (studyline.ReadLine() != null)
+                {
+                    Counter++;
+                }
+            }
+            Console.WriteLine($"The file contains {Counter} lines.");
+           
+        }
+    }
+
+}
+
