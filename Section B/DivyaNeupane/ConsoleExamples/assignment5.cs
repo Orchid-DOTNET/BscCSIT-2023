c08240674eead8190eda2b04912e8dee28551400
unknown <divyaneupane12@gmail.com>
2023-03-28 09:16:16 +0545

    Added assignment

---
+++ Section B/DivyaNeupane/ConsoleExamples/assignment5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Assignmet5
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("Enter first number:");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        try
+        {
+            
+            int divisionResult = firstNumber / secondNumber;
+            Console.WriteLine("Division of two numbers is: " + divisionResult);
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("An exception occurred: " + e.Message);
+        }
+
+        finally
+        {
+          
+            Console.WriteLine("Sum of two numbers is: " + (firstNumber + secondNumber));
+        }
+    }
+}
