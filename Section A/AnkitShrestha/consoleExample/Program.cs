6c09d3415931fd23091e1e005ae72737e342f729
Ankit Shrestha <ankit.shrestha196@gmail.com>
2023-03-28 19:45:30 +0545

    Complete assignment 5

--- Section A/AnkitShrestha/consoleExample/Program.cs
+++ Section A/AnkitShrestha/consoleExample/Program.cs
@@ -1,33 +1,44 @@
-﻿namespace ConsoleExamples
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            StringDoubleQuotes stringEdit = new StringDoubleQuotes();
-            //Assignment 1
-            stringEdit.adddoubleQuotes();
+﻿// namespace ConsoleExamples
+// {
+//     internal class Program
+//     {
+//         private static void Main(string[] args)
+//         {
+//             StringDoubleQuotes stringEdit = new StringDoubleQuotes();
+//             //Assignment 1
+//             stringEdit.adddoubleQuotes();
 
-            //Assignment 2
-            FileIO file = new FileIO();
-            file.fileOperation();
+//             //Assignment 2
+//             FileIO file = new FileIO();
+//             file.fileOperation();
 
-            //Assignment 3
-            LinkQExample linkq = new LinkQExample();
-            linkq.example();
+//             //Assignment 3
+//             LinkQExample linkq = new LinkQExample();
+//             linkq.example();
 
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
 // Assignment 4
 
-namespace MultipleInhertance {
+// namespace MultipleInhertance {
+//     internal class Program {
+//         private static void Main() {
+//             Calculate calculator = new Calculate();
+//             Console.WriteLine(calculator.add(3, 4));
+//         }
+//     }
+// }
+
+
+namespace ErrorHandlingApplication {
     internal class Program {
-        private static void Main() {
-            Calculate calculator = new Calculate();
-            Console.WriteLine(calculator.add(3, 4));
+        private static void Main(string[] args) {
+            DivNumbers d = new DivNumbers();
+            d.division(25, 0);
+            Console.ReadKey();
         }
     }
 }
