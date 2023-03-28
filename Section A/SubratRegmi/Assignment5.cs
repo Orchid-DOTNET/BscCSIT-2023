87e1b4ba6d82820d788cd85060e73be44c34748b
sregmi02 <regmisubrat02@gmail.com>
2023-03-28 17:21:43 +0545

    Assignment 5 Added

---
+++ Section A/SubratRegmi/Assignment5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class exceptionHandling
+    {
+        public void div(int x, int y)
+        {
+             try
+            {
+                double result = x / y;
+                Console.WriteLine("{0}/{1} = {2}", x, y, result);
+            }
+
+            catch(DivideByZeroException) {
+                Console.WriteLine("Exception Caught: {0} was divided by 0", x);
+            }
+
+
+        }
+    }
+}
--- Section A/SubratRegmi/Program.cs
+++ Section A/SubratRegmi/Program.cs
@@ -1,16 +1,18 @@
 ﻿using Assignment2;
 using Assignment1;
 using Assignment3;
-//assignment 1
-//stringHandling sh = new stringHandling();
-//sh.stringPrint();
-//assignment 2
-//fileExercise fe = new fileExercise();
-//fe.LineCount();
-//assignment 3
-//EmployeeDetails emp = new EmployeeDetails();
-//emp.empDetails();
-//assignment4
+using Assignment4;
+using Assignment5;
+/*assignment 1
+stringHandling sh = new stringHandling();
+sh.stringPrint();*/
+/*assignment 2
+fileExercise fe = new fileExercise();
+fe.LineCount();*/
+/*assignment 3
+EmployeeDetails emp = new EmployeeDetails();
+emp.empDetails();*/
+/*assignment4
 Calculator c = new Calculator();
 var addition = c.add(10, 2);
 var subtract = c.sub(10, 2);
@@ -20,3 +22,16 @@ Console.WriteLine("Addition: {0}",addition);
 Console.WriteLine("Subtraction: {0}", subtract);
 Console.WriteLine("Division: {0}",divide);
 Console.WriteLine("Multiplication: {0}", multiply);
+*/
+public class AssignmentsMain {
+    public static void Main()
+    {
+        exceptionHandling eh = new exceptionHandling();
+        int a,b;
+        Console.WriteLine("Enter the value of x: ");
+        a =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the value of y: ");
+        b = Convert.ToInt32(Console.ReadLine());
+        eh.div(a, b);
+    }
+}
