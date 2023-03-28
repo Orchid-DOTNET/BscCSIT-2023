bb68fc22f7e9eec936529a4fa528d047a0752073
bibishadahal <bibishadahal35@gmail.com>
2023-03-28 08:36:51 +0545

    Assignment 1, 2, 3,4 complete

---
+++ Section A/bibishadahal/consoleExample/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleExamples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            StringDoubleQuotes stringEdit = new StringDoubleQuotes();
+            //Assignment 1
+            stringEdit.adddoubleQuotes();
+
+            //Assignment 2
+            FileIO file = new FileIO();
+            file.fileOperation();
+
+            //Assignment 3
+            LinkQExample linkq = new LinkQExample();
+            linkq.example();
+
+
+        }
+    }
+}
+
+// Assignment 4
+
+namespace MultipleInhertance {
+    internal class Program {
+        private static void Main() {
+            Calculate calculator = new Calculate();
+            Console.WriteLine(calculator.add(3, 4));
+        }
+    }
+}
