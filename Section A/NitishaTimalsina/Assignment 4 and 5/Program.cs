db0662d72cb93f8a3fb08b1172a890a091ec0071
nitishat7 <nitishatimaslina7@gmail.com>
2023-03-28 18:52:45 +0545

    Assignment 5 added

---
+++ Section A/NitishaTimalsina/Assignment 4 and 5/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+/*Assignment 4 (Simple Calculator using Interfaces and MultipleInheritance)
+ 
+ * Assignment 5 (Exception Handling:DivideByZeroException) 
+ 
+ * Used the calculator from assignment 4 and caught the DivideByZero Exception in the same calculator
+*/
+using multipleInheritance;
+Calculator c = new Calculator();
+var addition=c.add(5, 6);
+var subtraction=c.sub(5, 5);
+var multiply=c.mul(5, 6);
+Console.WriteLine("Sum: {0}", addition);
+Console.WriteLine("Sub: {0}", subtraction);
+Console.WriteLine("Mul: {0}", multiply);
+
+Calculator d = new Calculator();
+
+d.div(15, 5);
+d.div(15, 0);
+
+
+
---
+++ Section A/NitishaTimalsina/Assignment 4 and 5/interface.cs
@@ -0,0 +1,28 @@
+﻿/*Assignment 4 (Simple Calculator using Interfaces and MultipleInheritance)
+ 
+ * Assignment 5 (Exception Handling:DivideByZeroException) 
+ 
+ * Used the calculator from assignment 4 and caught the DivideByZero Exception in the same calculator
+*/
+
+
+namespace multipleInheritance
+{
+    interface cal1
+    {
+        public int add(int x, int y);
+    }
+    interface cal2
+    {
+        public int sub(int x, int y);
+    }
+    interface cal3
+    {
+        public int mul(int x, int y);
+    }
+    interface cal4
+    {
+        public void div(int x, int y);
+    }
+}
+
