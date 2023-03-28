e54e775f0e3806cbf882b40c379690b68e682178
Ankit Rai <raia85859@gmail.com>
2023-03-28 21:08:42 +0545

    Updated

--- Section A/AnkitRai/Program.cs
+++ Section A/AnkitRai/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿//using Assignment3;
+using Assignment4;
+namespace ConsoleApplication
+{
+    class CodeRunner
+    {
+        public static void Main(string[] args)
+        {
+            //Assignment3.Employee.display_employee();
+              Calculation num = new Calculation();
+            Console.WriteLine(num.Add(5, 10));
+        }
+    }
+}
