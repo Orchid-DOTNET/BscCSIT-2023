3f4eb5e4d40e5bee9e2fa6f1e4f6f86af9868657
Bilish12 <bil.kharbuja100@gmail.com>
2023-03-28 14:15:01 +0545

    assignment 3 & 5 added

---
+++ Section B/BilishKharbuja/ConsoleExample/assignment3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+//Assigment 3 to display the Employee Details Whose Age is Greater Than 30 Order by ID
+
+namespace ConsoleExamples
+{
+    public class Employee
+    {
+        int id;
+        string name;
+        int age;
+        string email;
+        float salary;
+
+        public override string ToString()
+        {
+            return id + " " + name + " " + age + " " + salary;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+        {
+             new Employee {id=101, name="Bilish Kharbuja", age=28, email="biliskharbuja@gmail.com", salary=1000 },
+             new Employee {id=120, name="Bivek Chaudary", age=26, email="bivekchaudary@gmail.com", salary=2000 },
+             new Employee {id=106, name="Prayag Dhakal", age=38, email="dhakalprayag@gmail.com", salary=3000},
+             new Employee {id=116, name="Binaya Limbu", age=31, email="binaya@gmail.com", salary = 4000},
+             new Employee {id=123, name="Sudip Kc", age=33, email="sudipkc@gmail.com", salary=5000}
+        };
+
+            IEnumerable<Employee> orderedEmployeeList =
+                from emp in employees
+                where emp.age > 30
+                orderby emp.id
+                select emp;
+
+            Console.WriteLine("ID  Name  Age  Salary");
+            Console.WriteLine("=====================");
+            foreach (Employee o in orderedEmployeeList)
+            {
+                Console.WriteLine(o.ToString());
+            }
+            Console.WriteLine("=================");
+        }
+    }
+}
+
---
+++ Section B/BilishKharbuja/ConsoleExample/assignment5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionHandlingDemo
+{
+    class Assignment5
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 2;
+            int result;
+
+            try
+            {
+                // Attempt to divide num1 by num2, which will throw an exception if num2 is 0
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException ex)
+            {
+                // Handle the exception by printing an error message
+                Console.WriteLine("Error: Cannot divide by 0");
+            }
+            finally
+            {
+                // This code will always execute, regardless of whether or not an exception occurs
+                Console.WriteLine("Program execution complete.");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
