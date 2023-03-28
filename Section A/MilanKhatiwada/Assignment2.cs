344fca201b9943cc1c3616ad60da0e55301212ae
MILANKHATIWADA7 <milankhatiwada9840@gmail.com>
2023-03-28 07:55:08 +0545

    Added Assignment 1 , 2 , 3 ,4 and 5

---
+++ Section A/MilanKhatiwada/Assignment2.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+namespace Assignment2
+{
+    public class FileIO
+    {
+        public static void FileLineCount()
+        {
+            StreamReader file = new StreamReader("D:\\BscCSIT-2023\\Section A\\NarotsitKarki\\Assignment2.txt");
+            int count = 0;
+            string line = file.ReadLine();
+            while(line != null)
+            {
+                count++;
+                Console.WriteLine(line);
+                line = file.ReadLine();
+            }
+            Console.WriteLine("\n\n[*] Number of Lines: {0}", count);
+            
+        }
+    }   
+}
---
+++ Section A/MilanKhatiwada/Assignment5.cs
@@ -0,0 +1,27 @@
+using System;
+
+class program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 0;
+        int result;
+
+        try
+        {
+            result = a / b; 
+            Console.WriteLine("Result: " + result); 
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Exception caught: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Finally block executed.");
+        }
+
+        Console.ReadLine();
+    }
+}
