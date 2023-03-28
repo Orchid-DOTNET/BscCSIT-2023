5a179c3dd55c9da278f864b3ccea4ea05b83afa6
vidit-pandey <zoro123pandey@gmail.com>
2023-03-28 08:52:41 +0545

    Added Assignment 1 , 2, 3 and 4

--- Section A/ViditPandey/Program.cs
+++ Section A/ViditPandey/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Assignment2;
+using Assignment1;
+using Assignment3;
+
+using Assignment4;
+
+namespace ConsoleApplication
+{
+    class CodeRunner
+    {
+
+        
+        public static void Main(String[] args)
+        {
+            // Assignment 1 runner;
+            // StringEdit.EditString();
+
+            // Assignment 2 runner
+            //FileIO.FileLineCount();
+
+            // Assigment 3 Runner
+            //Console.WriteLine("Employee Information Display with Age greater than 30\n\n");
+            //Run_Assigment.Run_Assignment_3();
+
+            // Assignment 4 Runner
+            /* while(true){
+		       Menu.menu();
+	           }*/
+        }
+
+        
+    }
+}
---
+++ Section A/ViditPandey/assignment2.cs
@@ -0,0 +1,31 @@
+﻿
+// Assignment 2 File IO operations to read no of lines in a file
+
+using System;
+using System.IO;
+
+
+namespace Assignment2
+{
+    public class FileIO
+    {
+        public static void FileLineCount()
+        {
+            string path = "..\\..\\..\\Assignment2.txt";
+            try
+            {
+                
+                int line_count = File.ReadLines(path).Count();
+                Console.WriteLine("Number of Lines in file: {0}", line_count);
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Error File not found");
+        
+            }
+            
+        }
+
+    }   
+}
+
