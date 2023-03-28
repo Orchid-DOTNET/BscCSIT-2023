0b402fa24cf3ad1a2c6712ae6ea915a5a75c68c2
Ankit Rai <raia85859@gmail.com>
2023-03-28 20:41:03 +0545

    Assignment 5

---
+++ Section A/AnkitRai/Assignment5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Assignment5
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int num1 = 5, num2 = 0;
+            int result = num1 / num2;
+            Console.WriteLine(result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("End of program.");
+        }
+    }
+}
