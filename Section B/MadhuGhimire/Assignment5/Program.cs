cb5c19f7efb9b58495a0294c1912a9ac99c7a0d8
Madhu Ghimire <ghimiremadhu2058@gmail.com>
2023-03-28 22:19:36 +0545

    Assignment5 Added

---
+++ Section B/MadhuGhimire/Assignment5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int numerator = 10;
+        int denominator = 0;
+
+        try {
+            int result = numerator / denominator;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException ex) {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
