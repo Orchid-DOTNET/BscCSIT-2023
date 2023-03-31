9d734b1680aa74c97bb9fe8ba2882907b4cd94fc
limbubinaya <binayalimbu300@gmail.com>
2023-03-31 15:07:34 +0545

    first assignment

---
+++ Section B/BinayaLimbu/ConsoleApp1/Assignment1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class first
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter string: ");
+        string input = Console.ReadLine();
+
+        string[] StringWords = input.Split(' ');
+
+        for (int i = 0; i < StringWords.Length; i++)
+        {
+            StringWords[i] = "\"" + StringWords[i] + "\"";
+        }
+
+        string output = string.Join(" ", StringWords);
+
+        Console.WriteLine(output);
+    }
+}
