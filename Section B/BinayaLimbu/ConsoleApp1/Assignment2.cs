ebeb2edf2bc39e6ade9a90b29700bbe860dc04ad
limbubinaya <binayalimbu300@gmail.com>
2023-03-31 20:50:38 +0545

    Second assignment

---
+++ Section B/BinayaLimbu/ConsoleApp1/Assignment2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleExamples
+{
+    internal class SecondAssignment
+    {
+        static void Main()
+        {
+            string filePath = "C:\\Users\\acer\\Documents\\DOTNET\\BscCSIT-2023\\Section B\\BinayaLimbu\\ConsoleApp2\\texts.txt";
+
+            int LineCounter = 0;
+
+
+            using (StreamReader studyline = new StreamReader(filePath))
+            {
+                while (studyline.ReadLine() != null)
+                {
+                    LineCounter++;
+                }
+            }
+            Console.WriteLine($"The file contains {LineCounter} lines.");
+
+        }
+    }
+
+}
