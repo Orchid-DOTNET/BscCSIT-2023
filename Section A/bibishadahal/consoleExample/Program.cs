1683f5c000e46c86323fc0e72905637f9c1e0e30
bibishadahal <bibishadahal35@gmail.com>
2023-04-02 19:29:37 +0545

    Assignment 5  added

--- Section A/bibishadahal/consoleExample/Program.cs
+++ Section A/bibishadahal/consoleExample/Program.cs
@@ -1,33 +1,51 @@
-﻿namespace ConsoleExamples
-{
+﻿// namespace ConsoleExamples
+// {
+//     internal class Program
+//     {
+//         private static void Main(string[] args)
+//         {
+//             StringDoubleQuotes stringEdit = new StringDoubleQuotes();
+//             //Assignment 1
+//             stringEdit.adddoubleQuotes();
+
+//             //Assignment 2
+//             FileIO file = new FileIO();
+//             file.fileOperation();
+
+//             //Assignment 3
+//             LinkQExample linkq = new LinkQExample();
+//             linkq.example();
+            
+            
+
+
+//         }
+//     }
+// }
+
+// // Assignment 4
+
+// namespace MultipleInhertance {
+//     internal class Program {
+//         private static void Main() {
+//             Calculate calculator = new Calculate();
+//             Console.WriteLine(calculator.add(3, 4));
+//         }
+//     }
+// }
+
+
+// Assignment 5
+namespace ErrorHandlingApplication
+ {
     internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            StringDoubleQuotes stringEdit = new StringDoubleQuotes();
-            //Assignment 1
-            stringEdit.adddoubleQuotes();
+     {
+         private static void Main(string[] args)
+         {
 
-            //Assignment 2
-            FileIO file = new FileIO();
-            file.fileOperation();
-
-            //Assignment 3
-            LinkQExample linkq = new LinkQExample();
-            linkq.example();
-
-
-        }
-    }
-}
-
-// Assignment 4
-
-namespace MultipleInhertance {
-    internal class Program {
-        private static void Main() {
-            Calculate calculator = new Calculate();
-            Console.WriteLine(calculator.add(3, 4));
-        }
-    }
-}
+            DivNumbers d = new DivNumbers();
+            d.division(25, 0);
+            Console.ReadKey();
+         }
+     }
+ }
